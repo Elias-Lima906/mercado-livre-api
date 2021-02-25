61c1d84dbe05dbe0fc65b7fd56dac54316fb3894
Elias Lima <eliaslima900@hotmail.com>
2021-02-25 15:27:59 -0300

    add @Size input validation to define a minimum size to the password

--- src/main/java/br/com/api/ml/user/UserRequestDTO.java
+++ src/main/java/br/com/api/ml/user/UserRequestDTO.java
@@ -7,15 +7,15 @@ import javax.validation.constraints.Size;
 
 public class UserRequestDTO {
 
-	@NotBlank
-	@Email
+	@NotBlank(message = "O campo email não pode estar em branco!")
+	@Email(message = "O campo email deve estar num formato aceitável, ex: exemplo@mail.com")
 	private String email;
 
-	@NotBlank
+	@NotBlank(message = "O campo password não pode estar em branco!")
 	@Size(min = 6)
 	private String password;
 
-	public UserRequestDTO(String email, String password) {
+	public UserRequestDTO(@NotBlank @Email String email, @NotBlank String password) {
 		this.email = email;
 		this.password = password;
 	}
