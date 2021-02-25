bb725408d3af5b02e89ac21d2954b9700bddc0f7
Elias Lima <eliaslima900@hotmail.com>
2021-02-25 15:22:25 -0300

    remove feign dependency

--- src/main/java/br/com/api/ml/user/UserRequestDTO.java
+++ src/main/java/br/com/api/ml/user/UserRequestDTO.java
@@ -2,6 +2,7 @@ package br.com.api.ml.user;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 public class UserRequestDTO {
@@ -11,6 +12,7 @@ public class UserRequestDTO {
 	private String email;
 
 	@NotBlank
+	@Size(min = 6)
 	private String password;
 
 	public UserRequestDTO(String email, String password) {
