f20c39738c5f15ab6c98e80bdad8aae7819530de
Elias Lima <eliaslima900@hotmail.com>
2021-02-25 13:12:50 -0300

    initial commit

---
+++ src/test/java/br/com/api/ml/MercadoLiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
