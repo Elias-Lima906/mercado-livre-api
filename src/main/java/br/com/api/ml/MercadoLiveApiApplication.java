69e795c749b82aa9c19da89a7e8b76b55a2a86b9
Elias Lima <eliaslima900@hotmail.com>
2021-02-25 12:50:20 -0300

    initial commit

---
+++ src/main/java/br/com/api/ml/MercadoLiveApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.ml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLiveApiApplication.class, args);
+	}
+
+}
