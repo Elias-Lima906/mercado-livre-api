3b29d5bd4453d2ac97935ef63d4ec835ba60c28f
Elias Lima <eliaslima900@hotmail.com>
2021-02-25 15:08:15 -0300

    create user controller and post method

---
+++ src/main/java/br/com/api/ml/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.api.ml.user;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@PostMapping
+	@Transactional
+	public UserResponseDTO postMethodName(@RequestBody @Valid UserRequestDTO request) {
+
+		System.out.println(request.getPassword());
+		
+		@Valid
+		User user = request.toModel();
+		System.out.println(user.getPassword());
+		manager.persist(user);
+		return new UserResponseDTO(user);
+	}
+
+}
