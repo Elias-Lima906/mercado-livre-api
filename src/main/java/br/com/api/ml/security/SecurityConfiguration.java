28b2e6d1a8dfa7c97f55b972d0fe43482ed5660a
Elias Lima <eliaslima900@hotmail.com>
2021-03-02 17:40:35 -0300

    create and configurate class security config

---
+++ src/main/java/br/com/api/ml/security/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package br.com.api.ml.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.api.ml.user.UserRepository;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AutenticationService autenticationService;
+
+	@Autowired
+	private TokenService tokenService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+//	Configuração de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(autenticationService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+//	Configuração de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/users").permitAll()
+		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.antMatchers(HttpMethod.GET, "/products/**").permitAll()
+		.anyRequest().authenticated()
+		.and().cors()
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().addFilterBefore(new JwtRequestFilter(tokenService, userRepository),
+						UsernamePasswordAuthenticationFilter.class);
+	}
+
+//	Configuração de recursos estáticos
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**",
+				"/swagger-resources/**");
+	}
+
+}
