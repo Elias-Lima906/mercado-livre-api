0d351297079ad7343342c81feedca79fbfe12b3f
Elias Lima <eliaslima900@hotmail.com>
2021-03-08 17:41:18 -0300

    create equals and hashcode

--- src/main/java/br/com/api/ml/user/User.java
+++ src/main/java/br/com/api/ml/user/User.java
@@ -110,9 +110,46 @@ public class User implements UserDetails {
 
 	public static User findAuthenticatedUser(UserRepository userRepository) {
 
-		User usuarioLogado = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-		return userRepository.findById(usuarioLogado.getId()).get();
+		return userRepository.findById(loggedUser.getId()).get();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
 	}
 
 }
