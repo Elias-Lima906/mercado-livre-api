6c0f2d108e6bea40f305ada226497b1302c59ad2
Elias Lima <eliaslima900@hotmail.com>
2021-03-08 17:32:41 -0300

    create some convertion methods and business rules

--- src/main/java/br/com/api/ml/product/Product.java
+++ src/main/java/br/com/api/ml/product/Product.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,13 +24,20 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.api.ml.category.Category;
+import br.com.api.ml.category.CategoryResponseDTO;
 import br.com.api.ml.characteristc.Characteristic;
-import br.com.api.ml.characteristc.CharacteristicDTO;
+import br.com.api.ml.characteristc.CharacteristicRequestDTO;
+import br.com.api.ml.characteristc.CharacteristicResponseDTO;
+import br.com.api.ml.image.Image;
+import br.com.api.ml.image.ImageResponseDTO;
 import br.com.api.ml.user.User;
+import br.com.api.ml.user.UserRepository;
 
 @Entity
 public class Product {
@@ -69,14 +77,17 @@ public class Product {
 
 	@ManyToOne
 	private User user;
-	
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.MERGE)
+	private Set<Image> images = new HashSet<Image>();
+
 	@Deprecated
 	public Product() {
 	}
 
 	public Product(@NotBlank String name, @NotNull @Positive BigDecimal price,
 			@NotNull @Min(0) Integer availableQuantity, @NotBlank String description, @NotNull Category category,
-			@NotNull @Valid User user, @Size(min = 3) Collection<CharacteristicDTO> characteristics) {
+			@NotNull @Valid User user, @Size(min = 3) Collection<CharacteristicRequestDTO> characteristics) {
 		this.name = name;
 		this.price = price;
 		this.availableQuantity = availableQuantity;
@@ -84,11 +95,6 @@ public class Product {
 		this.category = category;
 		this.user = user;
 		this.characteristics.addAll(addCharacteristics(characteristics));
-	}
-
-	private Set<Characteristic> addCharacteristics(Collection<CharacteristicDTO> characteristicsDto) {
-		return characteristicsDto.stream().map(characteristic -> characteristic.toModel(this))
-				.collect(Collectors.toSet());
 	}
 
 	public Long getId() {
@@ -121,6 +127,62 @@ public class Product {
 
 	public Set<Characteristic> getCharacteristics() {
 		return characteristics;
+	}
+
+	public Set<Image> getImages() {
+		return images;
+	}
+
+	private Set<Characteristic> addCharacteristics(Collection<CharacteristicRequestDTO> characteristicsDto) {
+		return characteristicsDto.stream().map(characteristic -> characteristic.toModel(this))
+				.collect(Collectors.toSet());
+	}
+
+	public void saveImages(Set<String> links) {
+
+		for (String link : links) {
+			Image image = new Image(this, link);
+			if (images.contains(image)) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						"Já existe uma imagem com as mesmas informações associada a este úsuario!");
+			}
+
+			images.add(image);
+		}
+	}
+
+	public CategoryResponseDTO toCategoryDTO() {
+		return new CategoryResponseDTO(this.category);
+	}
+
+	public Set<CharacteristicResponseDTO> toCharacteristicDTO() {
+
+		return this.characteristics.stream()
+				.map(characteristic -> new CharacteristicResponseDTO(characteristic.getName(),
+						characteristic.getDescription()))
+				.collect(Collectors.toSet());
+
+	}
+
+	public Set<ImageResponseDTO> toResponseDTO() {
+		return this.images.stream().map(image -> new ImageResponseDTO(image)).collect(Collectors.toSet());
+	}
+
+	public boolean userIsOwner(UserRepository userRepository) {
+		User user = User.findAuthenticatedUser(userRepository);
+		return this.user.equals(user);
+	}
+
+	public static Product findProduct(EntityManager manager, Long id) {
+
+		Product product = manager.find(Product.class, id);
+
+		if (product == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+					"Não foi encontrado, em sua conta, nenhum produto com o id: " + id);
+		}
+
+		return product;
 	}
 
 }
