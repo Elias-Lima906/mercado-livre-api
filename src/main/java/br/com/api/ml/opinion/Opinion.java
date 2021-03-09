b71e5a29497060d53396c43131a5c669ccad7f0f
Elias Lima <eliaslima900@hotmail.com>
2021-03-09 12:58:04 -0300

    Create entity opinion to represent the opinions of a product

---
+++ src/main/java/br/com/api/ml/opinion/Opinion.java
@@ -0,0 +1,88 @@
+package br.com.api.ml.opinion;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import br.com.api.ml.product.Product;
+import br.com.api.ml.user.User;
+
+@Entity
+public class Opinion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Min(1)
+	@Max(5)
+	@Size(max = 1)
+	@Column(nullable = false, length = 1)
+	private Integer evaluation;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Title title;
+
+	@NotBlank
+	@Size(max = 500)
+	@Column(nullable = false, length = 500)
+	private String description;
+
+	@NotNull
+	@ManyToOne
+	private User user;
+
+	@NotNull
+	@ManyToOne
+	private Product product;
+
+	@Deprecated
+	public Opinion() {
+	}
+
+	public Opinion(@NotNull @Min(1) @Max(5) @Size(max = 1) Integer evaluation, @NotNull Title title,
+			@NotBlank @Size(max = 500) String description, @NotNull User user, @NotNull Product product) {
+		this.evaluation = evaluation;
+		this.title = title;
+		this.description = description;
+		this.user = user;
+		this.product = product;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Integer getEvaluation() {
+		return evaluation;
+	}
+
+	public Title getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+}
