03c678a2205ffc3e0ff5d554091910aa9a2f7d58
Elias Lima <eliaslima900@hotmail.com>
2021-03-10 11:57:50 -0300

    create a controller and a post method to save the question

---
+++ src/main/java/br/com/api/ml/question/QuestionController.java
@@ -0,0 +1,45 @@
+package br.com.api.ml.question;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.ml.product.Product;
+import br.com.api.ml.product.ProductResponseDTO;
+import br.com.api.ml.user.User;
+import br.com.api.ml.user.UserRepository;
+
+@RestController
+@RequestMapping("/productQuestions")
+public class QuestionController {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private Mailer mailer;
+
+	@PostMapping("/{id}")
+	public ProductResponseDTO postMethodName(@PathVariable Long id, @RequestBody @Valid QuestionRequestDTO request) {
+		@Valid User user = User.findAuthenticatedUser(userRepository);
+		@Valid Product product = Product.findProduct(manager, id);
+		Question question = request.toModel(user, product);
+
+		product.addQuestionToProduct(question);
+
+		mailer.sendQuestion(question, product, user);
+
+		return new ProductResponseDTO(product);
+	}
+
+}
