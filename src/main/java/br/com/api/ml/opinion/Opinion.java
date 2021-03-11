e52b1b62045c1804bee3433d8580cc393b081303
Elias Lima <eliaslima900@hotmail.com>
2021-03-11 16:08:23 -0300

    create functions to turn an entity object into a DTO, to check if exists equal opinions before save and add equals/hashcode

--- src/main/java/br/com/api/ml/opinion/Opinion.java
+++ src/main/java/br/com/api/ml/opinion/Opinion.java
@@ -1,19 +1,28 @@
 package br.com.api.ml.opinion;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Query;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.api.ml.product.Product;
 import br.com.api.ml.user.User;
@@ -79,12 +88,68 @@ public class Opinion {
 		return description;
 	}
 
-	public User getUser() {
-		return user;
+	public String getUserEmail() {
+		return user.getEmail();
 	}
 
 	public Product getProduct() {
 		return product;
+	}
+
+	public static Set<OpinionResponseDTO> toOpinionResponseDTO(Set<Opinion> opinions) {
+		return opinions.stream().map(opinion -> new OpinionResponseDTO(opinion)).collect(Collectors.toSet());
+	}
+
+	public void checkIfHasEqualOpinions(Set<Opinion> opinions) {
+		if (opinions.contains(this)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma opinião adicionada pelo úsuario: "
+					+ this.getUserEmail() + " a este produto, com o titulo: " + this.getTitle() + ".");
+		}
+	}
+
+	public List<?> findAllOpinions(EntityManager manager) {
+		Query query = manager.createQuery("SELECT o FROM Opinion o");
+		return query.getResultList();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((evaluation == null) ? 0 : evaluation.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Opinion other = (Opinion) obj;
+		if (evaluation == null) {
+			if (other.evaluation != null)
+				return false;
+		} else if (!evaluation.equals(other.evaluation))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (title != other.title)
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
 	}
 
 }
