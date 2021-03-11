26eda93d17a86170e89324b0d9be2aff09c05266
Elias Lima <eliaslima900@hotmail.com>
2021-03-11 16:21:52 -0300

    create functions to turn an entity object into a DTO and check if exists equal questions before save. Add equals/hashcode too

--- src/main/java/br/com/api/ml/question/Question.java
+++ src/main/java/br/com/api/ml/question/Question.java
@@ -1,6 +1,8 @@
 package br.com.api.ml.question;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +15,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.api.ml.product.Product;
 import br.com.api.ml.user.User;
@@ -62,6 +66,58 @@ public class Question {
 
 	public Product getProduct() {
 		return product;
+	}
+
+	public String getUserEmail() {
+		return user.getEmail();
+	}
+
+	public static Set<QuestionResponseDTO> toQuestionResponseDTO(Set<Question> questions) {
+		return questions.stream().map(question -> new QuestionResponseDTO(question)).collect(Collectors.toSet());
+	}
+
+	public void checkIfHasEqualQuestions(Set<Question> questions) {
+		if (questions.contains(this)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma pergunta feita pelo úsuario: "
+					+ this.getUserEmail() + " a este produto, com o titulo: " + this.getTitle() + ".");
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
 	}
 
 }
